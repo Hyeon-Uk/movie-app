d8d70c41b62b6c8b2f1a1fdc13e47318a176937b
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-01-31 23:07:58 +0900

    Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
-function Movie({id,year,title,summary,poster}){
+function Movie({year,title,summary,poster,genres}){
     return(
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title}/>
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <ul className="movie__genres">
+                    {
+                        genres.map((genre,index)=>(
+                        <li key={index} className="genres__genre">{genre}</li>
+                        ))
+                    }
+                </ul>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">
                     {summary}
                 </p>
             </div>
@@ -17,11 +24,11 @@ function Movie({id,year,title,summary,poster}){
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
